0288fd0bde3cda529d2ce95c7297060272c9f484
Marcel Ritter <ritterpfalz@gmail.com>
2022-08-25 16:59:04 +0200

    lebenslauf section

--- script.ts
+++ script.ts
@@ -4,6 +4,7 @@ function handleLoad(_event: Event) {
     console.log("hi");
     let viewPortfolioButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("viewPortfolioButton");
     viewPortfolioButton.addEventListener("click", viewPortfolio);
+
 }
 function viewPortfolio() {
     location.href = "#portfolioTitel";
