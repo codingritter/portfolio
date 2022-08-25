2af3e72559b5041ffa76fd9cb19b1b14e79bcdf6
Marcel Ritter <ritterpfalz@gmail.com>
2022-08-25 16:51:01 +0200

    page change

--- inhaltJson.js
+++ inhaltJson.js
@@ -1,12 +1,2 @@
 "use strict";
-var portfolio;
-(function (portfolio) {
-    loadArtikel("data.json");
-    async function loadArtikel(_url) {
-        let response = await fetch(_url);
-        let jsonArray = await response.json();
-        portfolio.kategorien = await JSON.parse(JSON.stringify(jsonArray));
-        portfolio.maintextAufrufen();
-    }
-})(portfolio || (portfolio = {}));
 //# sourceMappingURL=inhaltJson.js.map
--- script.js
+++ script.js
@@ -1,74 +1,11 @@
 "use strict";
-var portfolio;
-(function (portfolio) {
-    function maintextAufrufen() {
-        let uebermichHtml = document.getElementById("uebermich");
-        uebermichHtml.innerHTML = portfolio.kategorien.informationen.uebermich;
-        let uebermichButton = document.getElementById("ueberMichButton");
-        uebermichButton.addEventListener("click", uebermichKlick);
-        function uebermichKlick(_event) {
-            window.location.href = "#uebermichAnker";
-        }
-        let projekteButton = document.getElementById("projekteButton");
-        projekteButton.addEventListener("click", projekteKlick);
-        function projekteKlick(_event) {
-            window.location.href = "#projekteAnker";
-        }
-        let kontaktButton = document.getElementById("kontaktButton");
-        kontaktButton.addEventListener("click", kontaktKlick);
-        function kontaktKlick(_event) {
-            window.location.href = "#kontaktAnker";
-        }
-        let mailButton = document.getElementById("mail");
-        mailButton.addEventListener("click", mailKlick);
-        function mailKlick(_event) {
-            window.location.href = "mailto:marcelritterbuisan@gmail.com";
-        }
-        let kurseDiv = document.createElement("div");
-        kurseDiv.setAttribute("id", "inhaltDiv");
-        for (let i = 0; i < portfolio.kategorien.weiterbildungen.length; i++) {
-            let element = document.createElement("div");
-            element.setAttribute("id", "elementDiv");
-            kurseDiv.appendChild(element);
-            let name = document.createElement("h2");
-            name.innerHTML = portfolio.kategorien.weiterbildungen[i].name;
-            element.appendChild(name);
-            let bild = document.createElement("img");
-            bild.setAttribute("src", portfolio.kategorien.weiterbildungen[i].bild);
-            bild.addEventListener("click", bildKlick);
-            element.appendChild(bild);
-            let beschreibung = document.createElement("p");
-            beschreibung.setAttribute("id", "beschreibung");
-            beschreibung.innerHTML = portfolio.kategorien.weiterbildungen[i].beschreibung;
-            element.appendChild(beschreibung);
-            function bildKlick(_event) {
-                window.open(portfolio.kategorien.weiterbildungen[i].link, "_blank");
-            }
-        }
-        document.getElementById("kurse")?.appendChild(kurseDiv);
-        let inhaltDiv = document.createElement("div");
-        inhaltDiv.setAttribute("id", "inhaltDiv");
-        for (let i = 0; i < portfolio.kategorien.projekte.length; i++) {
-            let element = document.createElement("div");
-            element.setAttribute("id", "elementDiv");
-            inhaltDiv.appendChild(element);
-            let name = document.createElement("h2");
-            name.innerHTML = portfolio.kategorien.projekte[i].name;
-            element.appendChild(name);
-            let bild = document.createElement("img");
-            bild.setAttribute("src", portfolio.kategorien.projekte[i].bild);
-            bild.addEventListener("click", bildKlick);
-            element.appendChild(bild);
-            let beschreibung = document.createElement("p");
-            beschreibung.setAttribute("id", "beschreibung");
-            beschreibung.innerHTML = portfolio.kategorien.projekte[i].beschreibung;
-            element.appendChild(beschreibung);
-            function bildKlick(_event) {
-                window.open(portfolio.kategorien.projekte[i].link, "_blank");
-            }
-        }
-        document.getElementById("projekte")?.appendChild(inhaltDiv);
-    }
-    portfolio.maintextAufrufen = maintextAufrufen;
-})(portfolio || (portfolio = {}));
+window.addEventListener("load", handleLoad);
+function handleLoad(_event) {
+    console.log("hi");
+    let viewPortfolioButton = document.getElementById("viewPortfolioButton");
+    viewPortfolioButton.addEventListener("click", viewPortfolio);
+}
+function viewPortfolio() {
+    location.href = "#portfolioTitel";
+}
 //# sourceMappingURL=script.js.map
